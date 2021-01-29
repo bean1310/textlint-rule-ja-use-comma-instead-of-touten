8bb23426413cf80a33df60f94e0e15619eaad5b0
BEAN <22887835+bean1310@users.noreply.github.com>
2021-01-29 17:38:50 +0900

    [add] first impl

---
+++ src/textlint-rule-ja-use-comma-instead-of-touten.ts
@@ -0,0 +1,75 @@
+"use strict";
+
+import { TextlintRuleReporter } from "@textlint/types";
+import { RuleHelper } from "textlint-rule-helper";
+
+const japaneseRegExp = /(?:[々〇〻\u3400-\u4DBF\u4E00-\u9FFF\uF900-\uFAFF]|[\uD840-\uD87F][\uDC00-\uDFFF]|[ぁ-んァ-ヶ])/;
+const badComma = /[、\,]/;
+
+const reporter: TextlintRuleReporter = function (context)
+{
+    const {Syntax, RuleError, report, fixer, getSource} = context;
+    const ruleHelper = new RuleHelper(context);
+
+    return {
+        [Syntax.Str](node)
+        {
+            if (ruleHelper.isChildNode(node, [Syntax.Link, Syntax.Image, Syntax.BlockQuote, Syntax.Emphasis])) {
+                return;
+            }
+
+            const targetText = getSource(node);
+
+            if (!japaneseRegExp.test(targetText)) {
+                return;
+            }
+            
+            const validationResults = validateBadComma(targetText, badComma);
+
+            validationResults.forEach(result => {
+                if (!result.valid && result.index != undefined) {
+                    const ruleError = new RuleError("Disallow to use 読点", {
+                        index: result.index,
+                        fix: fixer.replaceTextRange([result.index, result.index + 1], "，")
+                    })
+                    report(node, ruleError);
+                }
+            });
+
+        }
+    }
+};
+
+/**
+ * 
+ * @param text Text for validation
+ * @param badComma Regular expression of disallow character
+ */
+function validateBadComma(text:string, badComma: RegExp) {
+    let baseIndex = 0;
+    let tmpText = text;
+    let result = [];
+    while(tmpText) {
+        if (badComma.test(tmpText)) {
+            const index = tmpText.search(badComma) + baseIndex;
+            result.push({
+                valid: false,
+                index: index
+            });
+            tmpText = tmpText.substr(index);
+            baseIndex += index;
+        } else {
+            result.push({
+                valid: true
+            });
+            break;
+        }
+    }
+    
+    return result;
+};
+
+module.exports = {
+    linter: reporter,
+    fixer: reporter
+};
---
+++ test/textlint-rule-ja-use-comma-instead-of-touten-test.ts
@@ -0,0 +1,45 @@
+const TextLintTester = require("textlint-tester");
+const tester = new TextLintTester();
+const rule = require("../src/textlint-rule-ja-use-comma-instead-of-touten");
+
+tester.run("use-comma-instead-of-touten", rule, {
+    valid: [
+        "今日は，いい天気だ．",
+        "それは，見ざる，言わざる，聞かざる．",
+        "Google, Apple, Facebook and Amazon"
+    ],
+    invalid: [
+        {
+            text: "明日は、雨だ．",
+            output: "明日は，雨だ．",
+            errors: [
+                {
+                    message: `Disallow to use 読点`,
+                    line: 1,
+                    column: 4
+                }
+            ]
+        },
+        {
+            text: "今日は、快晴だが，\n明日は、雨の予報があり、傘を持っていく必要があるだろう．",
+            output: "今日は，快晴だが，\n明日は，雨の予報があり，傘を持っていく必要があるだろう．",
+            errors: [
+                {
+                    message: `Disallow to use 読点`,
+                    line: 1,
+                    column: 4
+                },
+                {
+                    message: `Disallow to use 読点`,
+                    line: 2,
+                    column: 4
+                },
+                {
+                    message: `Disallow to use 読点`,
+                    line: 2,
+                    column: 12
+                }
+            ]
+        }
+    ]
+});
